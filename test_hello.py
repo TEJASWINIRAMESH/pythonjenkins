0b10101caf149b10a69815e8a0acdcc227512225
TEJASWINIRAMESH <115721265+TEJASWINIRAMESH@users.noreply.github.com>
2024-11-20 09:14:11 +0530

    Add files via upload

---
+++ test_hello.py
@@ -0,0 +1,10 @@
+# test_hello.py
+import unittest
+from hello import say_hello
+
+class TestHelloWorld(unittest.TestCase):
+    def test_say_hello(self):
+        self.assertEqual(say_hello(), "Hello, World!")
+
+if __name__ == "__main__":
+    unittest.main()
